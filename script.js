edf255d3cd06589e44004914675121013f48f9e8
Liana <lianavagyan.90@gmail.com>
2022-02-07 15:14:17 +0400

    feat: js file is completed and added

--- script.js
+++ script.js
@@ -10,12 +10,8 @@ fetch("https://restcountries.com/v3.1/all")
 
 function getData(data) {
   console.log(data);
-  const country = document.createElement("table");
+  const countryDiv = document.createElement("table");
   const row = document.createElement("tr");
-
-  //   const countryName = data.map((e) => {
-  //     return e.name.common;
-  //   });
 
   const headings = [
     "COUNTRY",
@@ -32,10 +28,31 @@ function getData(data) {
     th.style.border = "1px solid black";
     th.style.padding = "10px";
     row.append(th);
-    country.append(row);
+    countryDiv.append(row);
   }
-  country.style.border = "1px solid black";
-  country.style.width = "100px";
+  countryDiv.style.border = "1px solid black";
+  countryDiv.style.width = "100%";
 
-  MAIN_CONTAINER.append(country);
+  for (country of data) {
+    const currentRow = document.createElement("tr");
+    const td1 = initColumn(country.name.common);
+    const td2 = initColumn(country.flag);
+    const td3 = initColumn(country.capital);
+    const td4 = initColumn(country.population);
+    const td5 = initColumn(country.region);
+    const td6 = initColumn(country.area);
+    const td7 = initColumn(country.timezones);
+    currentRow.append(td1, td2, td3, td4, td5, td6, td7);
+    countryDiv.append(currentRow);
+  }
+
+  MAIN_CONTAINER.append(countryDiv);
+}
+function initColumn(text) {
+  const td = document.createElement("td");
+  td.textContent = text;
+  td.style.border = "1px solid black";
+  td.style.padding = "10px";
+  td.style.textAlign = "center";
+  return td;
 }
